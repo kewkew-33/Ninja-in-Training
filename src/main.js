4cd8349974f50fd6acfabd63b290fd671ad80f99
kewkew-33 <aidenmiku920@gmail.com>
2025-12-15 01:58:18 -0600

    Comment out style.css import in main.js
    Comment out the import statement for style.css.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './style.css';
+//import './style.css';
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 import { createEnvironment, createTargets } from './envionment.js';
 import * as TARGETS from './targets.js';
