ae369d8f56e56e3094f3cdc4b1d402bf20f89faf
kewkew-33 <aidenmiku920@gmail.com>
2025-12-15 02:51:28 -0600

    Rename import from THREE to T

--- src/targets.js
+++ src/targets.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import * as T from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 import { addScore } from './main.js';
 import { hitMark } from './main.js';
 import { FBXLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/FBXLoader.js';
