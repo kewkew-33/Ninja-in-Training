4cc54ae7fa2f313ededd8bf4796a21191711b39b
kewkew-33 <aidenmiku920@gmail.com>
2025-12-15 02:51:10 -0600

    Rename import from THREE to T in environment.js

--- src/envionment.js
+++ src/envionment.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import * as T from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 import * as TARGETS from './targets.js';
 
 export function createEnvironment(scene) {
